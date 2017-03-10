cced31b81cc8568d6ffa687ebf607bd43e4a9fcf
k2works <kakimomokuri@gmail.com>
2017-03-10 12:01:23 +0900

    Behavior-Driven Development Step02

--- spec/lib/idea_spec.rb
+++ spec/lib/idea_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 describe Idea do
   it "has a title" do
     idea = Idea.new
-    idea.title.should be_true
-    # or, alternatively: idea.title.should == true
+    expect(idea.title).to be_nil
   end
 end
